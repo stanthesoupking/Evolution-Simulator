d2107d6418546bf3e51f1d1229e9728c85b51529
StanTheSoupKing <stanthesoupking@gmail.com>
2015-11-15 18:11:18 +1100

    Added info

--- include/base_stimuli.h
+++ include/base_stimuli.h
@@ -1,6 +1,12 @@
 #pragma once
 #include "CStimulus.h"
 
+// Base-Stimuli header file:
+// Classes stored in here are the base stimuli that can be used by organisms.
+
+
+//SEnergyInRange Stimulus
+// - Is true when energy of owner is greater than parameter 'a' and less than prameter 'b'
 class SEnergyInRange: public CStimulus
 {
   public:

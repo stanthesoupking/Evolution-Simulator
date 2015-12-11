968716fb601cc70c5663b7e0df50793f8db79fe5
Spivee <spiveehere@gmail.com>
2015-12-11 15:34:23 +1100

    mem leak part 2
    CEngine now has a proper destructor handling its SDL objects

--- src/CEngine.cpp
+++ src/CEngine.cpp
@@ -5,9 +5,15 @@
 
 CEngine::CEngine( int sWidth, int sHeight, const char* title )
 {
-	SDL_Init( SDL_INIT_EVERYTHING );
+    SDL_Init( SDL_INIT_EVERYTHING );
 
     // Create a window.
     window = SDL_CreateWindow( title, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, sWidth, sHeight, SDL_WINDOW_SHOWN );
     renderer = new CRenderer( window );
+}
+
+CEngine::~CEngine ()
+{
+    SDL_DestrowWindow(window);
+    delete CRenderer;
 }
